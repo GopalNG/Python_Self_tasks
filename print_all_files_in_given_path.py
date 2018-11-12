74ad681fee4eb0da7d457768b5d523b36cab9f87
GopalN <nagubandigopal@gmail.com>
2018-11-12 20:25:14 +0530

    Update print_all_files_in_given_path.py

--- print_all_files_in_given_path.py
+++ print_all_files_in_given_path.py
@@ -1,16 +1,26 @@
-import os  #importing os module 
+Program Explanation or Flow:
+#importing os module @line 12
+#here os.listdir() : Returns a list of contained files and directory @line 14
+#printing the path given to know the present dir @line 16
+#A loop through the list returned by os.listdir() in a variable called files @line 17
+#os.path.join() it will join the filename with path passed files | to get all filesin that dir @line 17
+# now if it is a directory returned by os.path.join contained @ low_path variable @line 18
+# calling the self class with a path in low_path variable it prints all file names with in path @line 20
+# if it is not a directory it will retrun file name  @line 21
+#all this process continues till for loop end : --- len(files) 
+
+import os   
 
 def list_all(path):
-    files = os.listdir(path)   #here os.listdir() : Returns a list of contained files and directory
-    print(path)                #printing the path given 
-    for file in files:         #A loop through the list returned by os.listdir() in a variable called files
-        low_path = os.path.join(path,file) #os.path.join() it will join the filename with path passed files | to get all filesin that dir
-        #print(low_path)
-        if os.path.isdir(low_path): #now if it is a directory returned by os.path.join contained @ low_path variable 
-            list_all(low_path)     # calling the self class with a path in low_path variable it prints all file names with in path 
-        else:                      # if it is not a directory it will retrun file name 
+    files = os.listdir(path)   
+    print(path)               
+    for file in files:        
+        low_path = os.path.join(path,file)                   
+        if os.path.isdir(low_path):         
+            list_all(low_path)            
+        else:                             
             print("\t",file)     
-                                   #all this process continues till for loop end : --- len(files)
+                                         
 
 root = "D:/AA- phython-practice/flask_practice/section4/WebApiFlaskMysql"
 list_all(root)
